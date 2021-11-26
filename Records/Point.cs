fc705349611368958da13180daa56f0bb729f71b
Jasper Kent <software@jasperkent.com>
2021-11-26 08:51:21 +0000

    Add project files.

---
+++ Records/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Records
+{
+    public class PointC
+    {
+        public double X { get; init; }
+        public double Y { get; init; }
+        public double Z { get; init; }
+    }
+
+    public struct PointS
+    {
+
+        public double X { get; init; }
+        public double Y { get; init; }
+        public double Z { get; init; }
+    }
+
+    public record class PointR(double X, double Y, double Z)
+    {
+        public sealed override string ToString()
+        {
+            return "This is the base class.";
+        }
+    }
+
+    public readonly record struct PointRS(double X, double Y, double Z);
+
+    public record Point4D : PointR
+    {
+        public double CT { get; init; }
+
+        public Point4D(double X, double Y, double Z, double ct) 
+            : base(X, Y, Z)
+        {
+            CT = ct;
+        }
+    }
+}
---
+++ Records/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Records
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            PointR r1 = new PointR (1, 2, 3);
+            PointR r2 = new PointR (1, 2, 3);
+
+            PointS s1 = new PointS { X = 1, Y = 2, Z = 3 };
+            PointS s2 = new PointS { X = 1, Y = 2, Z = 3 };
+
+            PointC c1 = new PointC { X = 1, Y = 2, Z = 3 };
+            PointC c2 = new PointC { X = 1, Y = 2, Z = 3 };
+
+            PointRS rs1 = new PointRS(1, 2, 3);
+            PointRS rs2 = new PointRS(1, 2, 3);
+
+            Console.WriteLine(rs1);
+            Console.WriteLine(s1);
+
+            var point = new PointRS (X: 1.0, Y: 1.0, Z: 1.0);
+
+            var rs3 = rs2 with { Z = 200 };
+
+            var s3 = s2 with { Z = 200 };
+
+            var anon = new { X = 1, Y = 2, Z = 3 };
+
+            var anon2 = anon with { Z = 200 };
+
+            var p3d = new PointR(1, 2, 3);
+            var p4d = new Point4D(1, 2, 3, 4);
+
+            Console.WriteLine(p3d);
+            Console.WriteLine(p4d);
+
+            double x;
+ 
+            double z;
+
+            (x, double y, z) = p3d;
+
+            Console.WriteLine("Values:");
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+        }
+    }
+}
